9a7f436bc37382fffe6e64ff25c283b90d86476a
RealdoBeja98 <realdo.beja@mail.polimi.com>
2021-03-22 08:33:43 +0100

    aldos commit

---
+++ src/main/java/it/polimi/ingsw/AldosClass.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw;
+
+public class AldosClass {
+
+        public static void main( String[] args )
+        {
+            System.out.println( "Hello Realdo!" );
+        }
+
+}
