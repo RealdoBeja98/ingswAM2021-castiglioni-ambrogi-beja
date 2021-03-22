a0f4d0b4363a81ff3eacf745bee85004d0f4265e
puroidrogeno <puroidrogeno@gmail.com>
2021-03-22 08:27:40 +0100

    last test commit by andrea (wawiting for realdo)

---
+++ src/main/java/it/polimi/ingsw/test.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw;
+
+/**
+ * Hello world!
+ *
+ */
+public class test
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Realdo!" );
+    }
+}
+
