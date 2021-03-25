92a3e7a05559acc8ccf1121431770d3f1ef131bb
RealdoBeja98 <realdo.beja@mail.polimi.com>
2021-03-25 23:59:20 +0100

    warehosedepots and 3 prs

--- src/main/java/it/polimi/ingsw/PersonalBoard/WarehouseDepots.java
+++ src/main/java/it/polimi/ingsw/PersonalBoard/WarehouseDepots.java
@@ -11,6 +11,9 @@ public class WarehouseDepots {
     }
 
     public void addResource(Resource r, int pos) throws PositionAlreadyOccupiedException, ResourceAlreadyPlacedException {
+        if(pos >= 7){
+            throw new IndexOutOfBoundsException();
+        }
         if(resource[pos] != null){
             throw new PositionAlreadyOccupiedException();
         }
@@ -27,6 +30,37 @@ public class WarehouseDepots {
                 resource[pos] = r;
             }
         }
+    }
+
+    public void moveResource(int startPos, int finalPos) throws PositionAlreadyOccupiedException, ResourceAlreadyPlacedException {
+        if(finalPos >= 7){
+            throw new IndexOutOfBoundsException();
+        }
+        if(resource[finalPos] != null){
+            throw new PositionAlreadyOccupiedException();
+        }
+        else{
+            if(startPos == 0 && (finalPos ==1 || finalPos ==2) && ((resource[1] != null && resource[2] == null) || (resource[1] == null && resource[2] != null) || (resource[1] != null && resource[2] != null))) {
+                throw new ResourceAlreadyPlacedException();
+            } // non prende in considerazione se shelf 1 ha un coin, shelf two has 1 servant and blank, it can't change servant with coin }
+            else if((startPos == 1 || startPos == 2) && (finalPos == 3 || finalPos == 4 || finalPos == 5) && ((resource[3] != null && resource[4] !=null && resource[5] == null) ||
+                    (resource[3] == null && resource[4] != null && resource[5] != null) || (resource[3] != null && resource[4] != null && resource[5] != null) || (resource[3] != null || resource[4] != null || resource[5] != null))){
+                throw new ResourceAlreadyPlacedException();
+            }// non prende in considerazione se shelf 1 ha un coin, shelf two has 1 servant and blank, it can't change servant with coin
+            else if((startPos == 3 || startPos == 4 || startPos == 5) && (finalPos ==1 || finalPos ==2) && ((resource[1] != null && resource[2] == null) || (resource[1] == null && resource[2] != null) || (resource[1] != null && resource[2] != null))){
+                throw new ResourceAlreadyPlacedException();
+            }// non prende in considerazione se shelf 1 ha un coin, shelf two has 1 servant and blank, it can't change servant with coin
+            else{
+                resource[finalPos] = resource[startPos];
+                resource[startPos] = null;
+            }
+        }
+    }
+
+    public Resource removeResource(int pos){
+        Resource temp = resource[pos];
+        resource[pos] = null;
+        return temp;
     }
 }
 
