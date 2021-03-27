25806470d19dc746e96b7b8ae6314f8dcdd7d890
RealdoBeja98 <realdo.beja@mail.polimi.com>
2021-03-27 23:45:19 +0100

    addDevelopmentCard

---
+++ src/main/java/it/polimi/ingsw/PersonalBoard/SlotsDevelopmentCards.java
@@ -0,0 +1,66 @@
+package it.polimi.ingsw.PersonalBoard;
+
+import it.polimi.ingsw.PersonalBoard.Warehouse.PositionAlreadyOccupiedException;
+
+public class SlotsDevelopmentCards {
+    private DevelopmentCard[][]  slot = new DevelopmentCard[3][3];
+
+    private SlotsDevelopmentCards(){
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                slot[i][j] = null;
+            }
+        }
+    }
+
+    public void addDevelopmentCard(int pos, DevelopmentCard card) throws PositionAlreadyOccupiedException {
+        levelOfCard = checkLevelOfDevelopCard(card);
+        if (pos > 3) {
+             throw new IndexOutOfBoundsException();
+         }
+         else{
+             placeCard(levelOfCards, card);
+             }
+         }
+
+    private void placeCard(int levelOfCard, int pos, DevelopmentCard card) throws PositionAlreadyOccupiedException {
+        if (pos == 0) {
+            if (slot[pos][0] == null && levelOfCard == 0) {
+                occupySlot(pos, levelOfCard, card);
+            } else if (slot[pos][1] == null && levelOfCard == 1) {
+                occupySlot(pos, levelOfCard, card);
+            } else if (slot[pos][1] == null && levelOfCard == 2) {
+                occupySlot(pos, levelOfCard, card);
+            } else {
+                throw new PositionAlreadyOccupiedException();
+            }
+        } else if (pos == 1) {
+            if (slot[pos][0] == null && levelOfCard == 0) {
+                occupySlot(pos, levelOfCard, card);
+            } else if (slot[pos][1] == null && levelOfCard == 1) {
+                occupySlot(pos, levelOfCard, card);
+            } else if (slot[pos][1] == null && levelOfCard == 2) {
+                occupySlot(pos, levelOfCard, card);
+            } else {
+                throw new PositionAlreadyOccupiedException();
+            }
+        } else if (pos == 3) {
+            if (slot[pos][0] == null && levelOfCard == 0) {
+                occupySlot(pos, levelOfCard, card);
+            } else if (slot[pos][1] == null && levelOfCard == 1) {
+                occupySlot(pos, levelOfCard, card);
+            } else if (slot[pos][1] == null && levelOfCard == 2) {
+                occupySlot(pos, levelOfCard, card);
+            } else {
+                throw new PositionAlreadyOccupiedException();
+            }
+        }
+    }
+    private int checkLevelOfDevelopCard(DevelopmentCard card){
+          return card.getLevel;
+    }
+
+    private void occupySlot (int pos, int levelOfCard, DevelopmentCard card){
+          slot[pos][levelOfCard] = card;
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/PersonalBoard/PlaceAlredyTakenException.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.PersonalBoard;
+
+public class PlaceAlredyTakenException extends Throwable {
+}
