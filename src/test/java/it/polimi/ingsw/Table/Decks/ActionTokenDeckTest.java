777063f9a52214cae414a97c393b7012f47d071a
puroidrogeno <puroidrogeno@gmail.com>
2021-04-24 11:55:34 +0200

    fixed case test

--- src/test/java/it/polimi/ingsw/Table/Decks/ActionTokenDeckTest.java
+++ src/test/java/it/polimi/ingsw/Table/Decks/ActionTokenDeckTest.java
@@ -1,5 +1,5 @@
 package it.polimi.ingsw.Table.Decks;
-//import it.polimi.ingsw.Table.Decks.Token.ActionToken;
+import it.polimi.ingsw.Table.Decks.Token.ActionToken;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,13 +13,13 @@ class ActionTokenDeckTest {
     /**
      * This method tests drawing a card from the action token deck
      */
-    /*
+
     @Test
     void draw() {
         ActionTokenDeck deck = new ActionTokenDeck();
         ActionToken token = deck.draw();
         assertSame(token, deck.getDeck().get(deck.getDeck().size()-1));
     }
-    */
+
 
 }
