555afb35d81986f45626c804301cbc8441bf17cd
puroidrogeno <puroidrogeno@gmail.com>
2021-05-14 12:58:47 +0200

    improved cli automation

--- src/main/java/it/polimi/ingsw/Messages/ForwardMessages/PlacedDevelopmentCardForwardMessage.java
+++ src/main/java/it/polimi/ingsw/Messages/ForwardMessages/PlacedDevelopmentCardForwardMessage.java
@@ -3,6 +3,8 @@ package it.polimi.ingsw.Messages.ForwardMessages;
 import it.polimi.ingsw.Game.Game;
 import it.polimi.ingsw.Mains.ClientMain;
 import it.polimi.ingsw.Messages.ForwardMessage;
+import it.polimi.ingsw.View.Cli;
+import it.polimi.ingsw.View.View;
 
 import java.io.PrintWriter;
 
@@ -22,6 +24,8 @@ public class PlacedDevelopmentCardForwardMessage extends ForwardMessage {
     @Override
     public void execute(Game game, PrintWriter out) {
         ClientMain.getPlayerGame().addDevelopmentCard(nickname, position, developmentCard);
+        View w = new Cli();
+        w.showPBCurrent();
     }
 
     @Override
