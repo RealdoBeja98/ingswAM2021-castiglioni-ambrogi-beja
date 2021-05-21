949ad18615a2bc2df3d50768ec93a5cbf5a1f2cf
CarloAmbrogi <carlo.ambrogi@mail.polimi.it>
2021-05-21 16:08:39 +0200

    Added buttons and initial market tray

--- src/main/java/it/polimi/ingsw/Mains/GuiThread.java
+++ src/main/java/it/polimi/ingsw/Mains/GuiThread.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.Mains;
-import it.polimi.ingsw.Messages.GameMessages.ChooseNoActionLeaderCardGameMessage;
+import it.polimi.ingsw.Enums.LeaderWarehouse;
+import it.polimi.ingsw.Enums.NormalAction;
+import it.polimi.ingsw.Messages.GameMessages.*;
+import it.polimi.ingsw.Messages.Message;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -53,18 +56,46 @@ public class GuiThread extends Application implements Runnable{
         stage.show();
     }
 
-    private  void buttonTurn(Group root){
+    private void buttonTurn(Group root){
+        addButton(root, "_NO_A_LC", 625, 0, new ChooseNoActionLeaderCardGameMessage());
+        addButton(root, "_DIS_LC", 625, 35, new ChooseDiscardLeaderCardGameMessage());
+        addButton(root, "_PLAY_LC", 625, 70, new ChoosePlayLeaderCardGameMessage());
+        addButton(root, "_NA_TAKE", 625, 105, new SelectNormalActionGameMessage(NormalAction.TAKE_RESOURCES_FROM_THE_MARKET));
+        addButton(root, "_NA_BUY", 625, 140, new SelectNormalActionGameMessage(NormalAction.BUY_DEVELOPMENT_CARD));
+        addButton(root, "_NA_PROD", 625, 175, new SelectNormalActionGameMessage(NormalAction.ACTIVATE_PRODUCTION));
+        addButton(root, "_END_T", 625, 210, new EndTurnGameMessage());
 
-        Button b = new Button("_NO_A_LC");
+        addButton(root, "C11", 55, 50, new BuyDevelopmentCardGameMessage(1, 1));
+        addButton(root, "C12", 205, 50, new BuyDevelopmentCardGameMessage(1, 2));
+        addButton(root, "C13", 355, 50, new BuyDevelopmentCardGameMessage(1, 3));
+        addButton(root, "C14", 505, 50, new BuyDevelopmentCardGameMessage(1, 4));
+        addButton(root, "C21", 55, 280, new BuyDevelopmentCardGameMessage(2, 1));
+        addButton(root, "C22", 205, 280, new BuyDevelopmentCardGameMessage(2, 2));
+        addButton(root, "C23", 355, 280, new BuyDevelopmentCardGameMessage(2, 3));
+        addButton(root, "C24", 505, 280, new BuyDevelopmentCardGameMessage(2, 4));
+        addButton(root, "C41", 55, 510, new BuyDevelopmentCardGameMessage(3, 1));
+        addButton(root, "C42", 205, 510, new BuyDevelopmentCardGameMessage(3, 2));
+        addButton(root, "C43", 355, 510, new BuyDevelopmentCardGameMessage(3, 3));
+        addButton(root, "C44", 505, 510, new BuyDevelopmentCardGameMessage(3, 4));
+
+        addButton(root, "_S_BP", 850, 450, new SelectDefaultProductionPowerGameMessage());
+        addButton(root, "_S_P1", 942, 450, new SelectProductionDevelopmentCardGameMessage(1));
+        addButton(root, "_S_P2", 1052, 450, new SelectProductionDevelopmentCardGameMessage(2));
+        addButton(root, "_S_P3", 1162, 450, new SelectProductionDevelopmentCardGameMessage(3));
+    }
+
+    private void addButton(Group root, String buttonName, int x, int y, Message message){
+        Button b = new Button(buttonName);
         EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e)
             {
-                out.println(new ChooseNoActionLeaderCardGameMessage());
+                out.println(message);
             }
         };
 
         b.setOnAction(event);
-        b.setLayoutX(625);
+        b.setLayoutX(x);
+        b.setLayoutY(y);
 
         TilePane r = new TilePane();
         root.getChildren().add(b);
--- src/main/java/it/polimi/ingsw/View/Gui.java
+++ src/main/java/it/polimi/ingsw/View/Gui.java
@@ -1,7 +1,10 @@
 package it.polimi.ingsw.View;
 
+import it.polimi.ingsw.Enums.Resource;
+import it.polimi.ingsw.Game.PlayerGame;
 import it.polimi.ingsw.Mains.ClientMain;
 import it.polimi.ingsw.Mains.GuiThread;
+import it.polimi.ingsw.Table.Decks.Leader.LeaderCard;
 import javafx.application.Platform;
 import javafx.scene.Group;
 import javafx.scene.canvas.Canvas;
@@ -9,6 +12,8 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class Gui extends View{
 
@@ -29,7 +34,61 @@ public class Gui extends View{
 
     @Override
     public void showMarket() {
-
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 4; j++){
+                String name = "NullMarble";
+                Resource marble = ClientMain.getPlayerGame().getMarket().getMarketTray()[i][j].getWhatIAm();
+                if(marble == null){
+                    name = "NullMarble";
+                } else {
+                    switch (marble){
+                        case COIN: name = "CoinMarble";
+                            break;
+                        case FAITH: name = "FaithMarble";
+                            break;
+                        case SERVANT: name = "ServantMarble";
+                            break;
+                        case SHIELD: name = "ShieldMarble";
+                            break;
+                        case STONE: name = "StoneMarble";
+                            break;
+                        case WHITE: name = "WhiteMarble";
+                            break;
+                    }
+                }
+                String fullName = "Marbles/" + name + ".png";
+                drawMarble(gc, j * 80, i * 80 + 690, fullName);
+            }
+        }
+        String name = "NullMarble";
+        Resource extraMarble = ClientMain.getPlayerGame().getMarket().getExtraMarble().getWhatIAm();
+        if(extraMarble == null){
+            name = "NullMarble";
+        } else {
+            switch (extraMarble) {
+                case COIN:
+                    name = "CoinMarble";
+                    break;
+                case FAITH:
+                    name = "FaithMarble";
+                    break;
+                case SERVANT:
+                    name = "ServantMarble";
+                    break;
+                case SHIELD:
+                    name = "ShieldMarble";
+                    break;
+                case STONE:
+                    name = "StoneMarble";
+                    break;
+                case WHITE:
+                    name = "WhiteMarble";
+                    break;
+            }
+        }
+        String fullName = "Marbles/" + name + ".png";
+        drawMarble(gc, 320, 690, fullName);
     }
 
     @Override
@@ -52,7 +111,28 @@ public class Gui extends View{
     @Override
     public void showPersonalBoard() {
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        gc.fillText("aaaaaaaaaaaa", 1000, 500);
+
+        int navigatorBoard = 0;
+        ArrayList<PlayerGame.PlayerPlayer> players = ClientMain.getPlayerGame().getPlayers();
+        for(PlayerGame.PlayerPlayer n : players){
+            String nickname = n.getNickname();
+            if(nickname.equals(ClientMain.getClientNick())){
+                gc.fillText(nickname, 700, 20);
+
+            } else {
+                if(navigatorBoard == 0){
+                    gc.fillText(nickname, 1260, 20);
+                }
+                if(navigatorBoard == 1){
+                    gc.fillText(nickname, 700, 510);
+                }
+                if(navigatorBoard == 2){
+                    gc.fillText(nickname, 1260, 510);
+                }
+                navigatorBoard++;
+            }
+        }
+
     }
 
     @Override
@@ -63,6 +143,11 @@ public class Gui extends View{
     private void drawCards(GraphicsContext gc, int x, int y, String name) {
         Image img = new Image(name);
         gc.drawImage(img, x, y, 150, 230);
+    }
+
+    private void drawMarble(GraphicsContext gc, int x, int y, String name) {
+        Image img = new Image(name);
+        gc.drawImage(img, x, y, 80, 80);
     }
 
 }
